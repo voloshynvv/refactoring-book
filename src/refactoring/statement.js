a42c1e712f6e6326fdbbd797d89e68c5ae3c5c05
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2025-12-15 17:30:54 +0100

    extract formatting into seperate function

--- src/refactoring/statement.js
+++ src/refactoring/statement.js
@@ -7,21 +7,15 @@ export function statement(invoice, plays) {
 
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
   return result;
@@ -73,6 +67,14 @@ export function statement(invoice, plays) {
     }
 
     return result;
+  }
+
+  function usd(amount) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(amount / 100);
   }
 }
 
