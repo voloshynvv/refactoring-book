fae0951fa6db70cc9f81deda7581fa30bf3e674a
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2025-12-15 17:16:56 +0100

    refactoring: extract method

---
+++ src/refactoring/statement.js
@@ -0,0 +1,70 @@
+import invoices from "./invoices.json";
+import plays from "./plays.json";
+
+export function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    // print line for this order
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
+  }
+
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+
+  return result;
+
+  function playFor(performance) {
+    return plays[performance.playID];
+  }
+
+  function amountFor(performance) {
+    let result = 0;
+
+    switch (playFor(performance).type) {
+      case "tragedy":
+        result = 40000;
+
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+
+        break;
+
+      case "comedy":
+        result = 30000;
+
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+
+        result += 300 * performance.audience;
+
+        break;
+
+      default:
+        throw new Error(`unknown type: ${playFor(performance).type}`);
+    }
+
+    return result;
+  }
+}
+
+console.log(statement(invoices[0], plays));
